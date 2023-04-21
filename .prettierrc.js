3566b8feb517299d96f5b9b3dde2f079db3346e7
Kafanal Kafi <59825140+kkafi09@users.noreply.github.com>
2023-04-21 13:03:12 +0700

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const config = {
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  endOfLine: 'auto'
+};
+
+module.exports = config;
